7bb43a69f0aaee68bd94e872e2ecb696401d91f7
Pranav Ravichandran <prp.1111@gmail.com>
2012-09-23 12:52:27 +0530

    Extracting methods and parameters and writing to generated file.

--- mapper.c
+++ mapper.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include <malloc.h>
 #include "lexMap/keyMapper.h"
 #include "regex.h"
@@ -15,41 +16,28 @@ int main(int argc, char *argv[])
 	char delim[] = ",\n";
 	
 	char *s;
-	//s = malloc(sizeof(char*));
-	s = lex("target.txt", "key.txt", "mapped.txt");
-	//printf("%s\n", s);
+	lex("target.txt", "key.txt", "mapped.txt");
+	s = targetFile_read("mapped.txt");
 	split = splitByDelim(s, delim);
 
 	char *pattern = "((?:[a-z][a-z0-9_]*))(\\s*)(\\()(\\s*)(.*)(\\s*)(\\))";
-
-	//char *stub = targetFile_read("key.txt");
-
-	//printf("%s", stub);
-
-	/*printf("\n[%s]\n", restr.substr[4]);
-	printf("%s\n", trimwhitespace(split[4]));
-	printf("\n%d\n", tokenized);*/
-
+	char *stub = targetFile_read("./stub/stub.c");
 	int count = 0;
 
 	for (; count < tokenized; count++) {
-		printf("\n%d\n", count);
-		printf("%s\n", split[count]);
 		split[count] = trimwhitespace(split[count]);
-		printf("%s\n", split[count]);
 		regexStr restr = regexMatch(pattern, trimwhitespace(split[count]));
-		//printf("%s\n", restr.substr[4]);
-		//strcat(stub, restr.substr[0]);
-		//strcat(stub, "(");
-	        //strcat(stub, restr.substr[4]);
-	        //strcat(stub, ");\n");
+		strcat(stub, restr.substr[METHOD]);
+		strcat(stub, "(");
+	        strcat(stub, restr.substr[PARAM]);
+	        strcat(stub, ");\n");
 	}
 
-	//strcat(stub, "return 0;\n}");
+	strcat(stub, "return 0;\n}");
 
-	//printf("\n%s\n", stub);
-	
-	free(s);
+	targetFile_write("gen.c", stub);
+	system("gcc gen.c -o gen helpers.h");
+	system("./gen");
 	
 	return 0;
 }
